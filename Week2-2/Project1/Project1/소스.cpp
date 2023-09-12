49d2c064fedb5c4c16c3fbc85a25b6c27626d49e
unknown <applespeed550@gmail.com>
2023-09-12 23:12:29 +0900

    2주차2번쨰 과제

--- Week2-2/Project1/Project1/소스.cpp
+++ Week2-2/Project1/Project1/소스.cpp
@@ -1,40 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <conio.h>
+#include <cstdio>
+
 using namespace std;
 
-	int main() {
-		char input;
+int main() {
+	char input[5];
 
-		printf("      202207016\n");
-		printf("        임완희\n");
-		printf("      Application\n");
+	printf("    임완희의 행복한 VC++\n");
 
-		while (1) {
-			printf("q, w, e, r중에 입력하세요\n");
-			input = _getch();
 
-			if (input == 113) {
-				printf("나는 과제를 겁나 잘하는 학생이다\n");
-				printf("\n");
-			}
-			else if (input == 119) {
-				printf("나는 과제가 너무 즐겁다\n");
-				printf("\n");
-			}
-			else if (input == 101) {
-				printf("교수님 과제 더 내주세요\n");
-				printf("\n");
-			}
+	while (1) {
+		printf("command>");
+		gets_s(input);
 
-			else if (input == 114) {
-				printf("교수님 과제 더 내주세요\n");
-				printf("\n");
-			}
-
-			else if (input == 27) {
-				break;
-			}
+		if (strcmp(input, "q") == 0) {
+			printf("qwer화이팅\n");
+			printf("\n");
+		}
+		else if (strcmp(input, "w") == 0) {
+			printf("과제 너무 좋다\n");
+			printf("\n");
+		}
+		else if (strcmp(input, "e") == 0) {
+			printf("담주부턴 과제량 3배다?\n");
+			printf("\n");
+		}
+		else if (strcmp(input, "r") == 0) {
+			printf("행복합니다...\n");
+			printf("\n");
+		}
+		/*
+		else if (input[0] == 27) {
+			break; 
+		}
+		미구현... 1시간동안 찾아봤는데 몰르겠어요...
+		*/
+		else {
+			printf("qwer중 입력해주세요");
 		}
 	}
 }
